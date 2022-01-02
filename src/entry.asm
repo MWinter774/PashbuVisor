90cbf22111e936c6a35059099670ded05d118f2e
Maor Winter <winter.maor@gmail.com>
2022-01-02 22:51:37 +0200

    Function to switch to protected mode

--- src/entry.asm
+++ src/entry.asm
@@ -1,11 +1,15 @@
-%include "protected_mode.asm"
-
 [org 0x7c00] ; bootloader offset
 mov bp, 0x9000 ; set the stack
 mov sp, bp
 
 call switch_to_protected_mode
-jmp $ ; this will actually never be executed
+jmp entry
+
+%include "protected_mode.asm"
+
+[bits 32]
+entry:
+    jmp $
 
 ; bootsector
 times 510-($-$$) db 0

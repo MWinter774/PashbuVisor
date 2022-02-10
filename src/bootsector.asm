38ed9f4b33301c84065ec960ab7b23ac7557508b
Maor Winter <winter.maor@gmail.com>
2022-02-10 20:39:36 +0200

    Implemented disk_load function

--- src/bootsector.asm
+++ src/bootsector.asm
@@ -1,15 +1,30 @@
 [org 0x7c00] ; bootloader offset
-mov bp, 0x9000 ; set the stack
-mov sp, bp
+KERNEL_OFFSET equ 0x1000
+    mov [BOOT_DRIVE], dl
+    mov bp, 0x9000 ; set the stack
+    mov sp, bp
 
-call switch_to_protected_mode
-jmp entry
+    call load_kernel
+    call switch_to_protected_mode
+    jmp entry
 
+%include "bootsector_disk_load.asm"
 %include "protected_mode.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
 
 [bits 32]
 entry:
+    call KERNEL_OFFSET
     jmp $
+
+BOOT_DRIVE db 0
 
 ; bootsector
 times 510-($-$$) db 0

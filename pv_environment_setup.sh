4a2ee0f6972f99d5a9946df4c372a277827ab344
Maor Winter <winter.maor@gmail.com>
2022-02-10 20:49:59 +0200

    Fixed emulation command

--- pv_environment_setup.sh
+++ pv_environment_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 alias build="mkdir ./out 2>/dev/null | make -f ./build/Makefile"
-alias run="qemu-system-x86_64 out/output.bin"
+alias run="qemu-system-x86_64 -fda out/os-image.bin"
 alias build_and_run="build && run"

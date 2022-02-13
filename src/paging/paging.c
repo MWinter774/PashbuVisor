06b4beb04d271d7ab33ad3f9426029ace2df8207
Maor Winter <winter.maor@gmail.com>
2022-02-13 10:44:44 +0200

    Documented setup_page_directory

--- src/paging/paging.c
+++ src/paging/paging.c
@@ -6,6 +6,10 @@ void setup_page_directory()
 {
     for (int i = 0; i < 1024; i++)
     {
+        // Page table pointer: 0x00000000
+        // Supervisor: only the kernel can access this page table
+        // Read/Write: Read/Write access to this page table
+        // Not Present: This page table is not present
         page_directory[i] = 0x00000002;
     }
 }

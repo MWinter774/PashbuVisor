7a5e2b304d0419c8c80506e8d0ffb3bd9845a699
Maor Winter <winter.maor@gmail.com>
2022-02-13 09:34:32 +0200

    Alias for make clean

--- pv_environment_setup.sh
+++ pv_environment_setup.sh
@@ -3,3 +3,5 @@
 alias build="mkdir ./out 2>/dev/null | make -f ./build/Makefile"
 alias run="qemu-system-x86_64 -fda out/os-image.bin"
 alias build_and_run="build && run"
+alias clean_out="make clean -f ./build/Makefile"
+

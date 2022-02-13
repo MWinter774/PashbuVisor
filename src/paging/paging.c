2c23af6314315dedbd7524950ce5a99b23709a25
Maor Winter <winter.maor@gmail.com>
2022-02-13 16:36:18 +0200

    Enabled PAE

--- src/paging/paging.c
+++ src/paging/paging.c
@@ -1,9 +1,13 @@
 #include <paging/paging.h>
-#define PAGE_DIRECTORY_SIZE (1024)
+#define PAGE_DIRECTORY_POINTER_TABLE_SIZE (4)
+#define PAGE_DIRECTORY_SIZE (512)
 #define PAGE_TABLE_SIZE (1024)
 
+// The PDPT, each entry holds pointer to a page directory. This is in order to enable PAE.
+unsigned long page_directory_pointer_table[PAGE_DIRECTORY_POINTER_TABLE_SIZE] __attribute__((aligned(32)));
+
 // The Page Directory table, each entry holds pointer to a page table and some flags
-unsigned int page_directory[PAGE_DIRECTORY_SIZE] __attribute__((aligned(4096)));
+unsigned long page_directory[PAGE_DIRECTORY_SIZE] __attribute__((aligned(4096)));
 
 // A Page Table, each entry holds pointer to physical memory and some flags
 unsigned int page_table[PAGE_TABLE_SIZE] __attribute__((aligned(4096)));
@@ -13,10 +17,10 @@ unsigned int page_table[PAGE_TABLE_SIZE] __attribute__((aligned(4096)));
  */
 void load_page_directory(void * page_directory)
 {
-    __asm__("movl %%cr3, %0\n;"
-            :
-            : "r"(page_directory)
-            :);
+    asm volatile("movl %%cr3, %0\n;"
+                :
+                : "r"(page_directory)
+                :);
 }
 
 /*
@@ -24,15 +28,28 @@ void load_page_directory(void * page_directory)
  */
 void enable_paging()
 {
-    __asm__("mov %eax, %cr0\n\t"
-            "or $0x80000000, %eax\n\t"
-            "mov %cr0, %eax\n\t");
+    asm volatile(   "mov %eax, %cr0\n;"
+                    "or $0x80000000, %eax\n;"
+                    "mov %cr0, %eax\n;");
+}
+
+/*
+ * Set the 5th bit in CR4 to enable PAE.
+ */
+void enable_pae()
+{
+    asm volatile (  "movl %%cr4, %%eax\n;"
+                    "bts $5, %%eax\n;"
+                    "movl %%eax, %%cr4\n;"
+                    :
+                    :
+                    : "eax");
 }
 
 /*
  * Initializes a page directory table. page_directory_size SHOULD BE 1024!
  */
-void setup_page_directory(unsigned int * page_directory, unsigned int page_directory_size)
+void setup_page_directory(unsigned long * page_directory, unsigned int page_directory_size)
 {
     for (unsigned int i = 0; i < page_directory_size; i++)
     {
@@ -60,9 +77,16 @@ void initialize_paging()
     setup_page_directory(page_directory, sizeof(page_directory));
     setup_page_table(page_table, sizeof(page_table));
 
-    // Page directory holds only one page table
-    page_directory[0] = ((unsigned int)page_table) | 3;
+    page_directory_pointer_table[0] = ((unsigned long)page_directory) | 1;
 
+    // Page directory holds only one page table.
+    // This page table is:
+    // Present
+    // Read/Write
+    // 2MiB (not 4KiB which is 32 bit)
+    page_directory[0] = ((unsigned int)page_table) | 0b10000011;
+
+    enable_pae();
     load_page_directory(page_directory);
     enable_paging();
 }

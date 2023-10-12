3d2bcdc5ca03470a64a23c7604239f94df311df9
Maor Winter <winter.maor@gmail.com>
2023-10-12 16:46:10 +0300

    Created stack

--- src/kernel/boot.asm
+++ src/kernel/boot.asm
@@ -2,7 +2,15 @@ global start
 
 section .text
 bits 32
+
 start:
+    mov esp, stack_top
+
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+section .bss
+stack_bottom:
+    resb 64
+stack_top:
